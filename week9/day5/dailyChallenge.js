ec283a9f919ca4e745aba4e53c33c6e68d786c9f
Nirmal <Nirmal@admin.com>
2023-12-21 18:10:41 +0400

    new

---
+++ week9/day5/dailyChallenge.js
@@ -0,0 +1,20 @@
+function isAnagram(str1, str2) {
+    // Trim whitespace and  lowercase
+    let normalizedStr1 = str1.trim().toLowerCase();
+    let normalizedStr2 = str2.trim().toLowerCase();
+
+    // Sort the characters in each string
+    let sortedStr1 = normalizedStr1.split('').sort().join('').trim();
+    let sortedStr2 = normalizedStr2.split('').sort().join('').trim();
+
+    // Compare the sorted strings
+    if (sortedStr1==sortedStr2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isAnagram("Astronomer", "Moon starer")); // true
+console.log(isAnagram("School master", "The classroom")); // true
+console.log(isAnagram("The Morse Code", "Here come dots")); // true
